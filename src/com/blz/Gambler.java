646f60dc06b5b07707ddaca1c0e3055650297544
KumarNavin <navins398@gmail.com>
2023-01-06 23:05:43 +0530

    uc1 WELCOME

--- src/com/blz/Gambler.java
+++ src/com/blz/Gambler.java
@@ -1,7 +1,11 @@
 package com.blz;
 
 public class Gambler {
+    public static final int BET = 1;
+    public static final int AMOUNT = 100;
     public static void main(String[] args) {
-        System.out.println("Welcome");
+
+        int won = 0;
+        System.out.println("Initial Stake is " +AMOUNT+ "$ and bet is " +BET+"$");
     }
 }
